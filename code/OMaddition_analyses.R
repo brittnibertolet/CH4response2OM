fa2d7bcf27a1a74a6922660a7fb1314c5d6aadba
Brittni Bertolet <brittnibertolet@gmail.com>
2022-03-18 09:41:25 -0700

    Updated analyses for revisions

--- code/OMaddition_analyses.R
+++ code/OMaddition_analyses.R
@@ -211,24 +211,28 @@ ggsave("github/figures/SuppFigure1.pdf", height=5.5, width=9)
 
 # Figure 2
 nonMCC=ggplot(DF, aes(x=B.axis1, y=B.axis2))+geom_point(aes(color=sediment_pH, size=percOM))+
-  xlab("non-MCC PCoA 1 [29.2%]")+
-  ylab("non-MCC PCoA 2 [11.6%]")+
+  xlab("PCoA 1 [29.2%]")+
+  ylab("PCoA 2 [11.6%]")+
   theme_bw()+
+  ggtitle("Non-methanogen\ncommunity")+
   theme(panel.grid=element_blank(),
-        legend.box = "horizontal")
+        legend.box = "horizontal",
+        plot.title = element_text(hjust = 0.5))
 MCC=ggplot(DF, aes(x=M.axis1, y=M.axis2))+geom_point(aes(color=sediment_pH, size=percOM))+
-  xlab("MCC PCoA 1 [40.6%]")+
-  ylab("MCC PCoA 2 [12.6%]")+
+  xlab("PCoA 1 [40.6%]")+
+  ylab("PCoA 2 [12.6%]")+
+  ggtitle("Methanogen\ncommunity")+
   scale_size_continuous(name="OM %")+
   scale_color_continuous(name="pH")+
   theme_bw()+
   theme(panel.grid=element_blank(),
-        legend.box = "horizontal")
+        legend.box = "horizontal",
+        plot.title = element_text(hjust = 0.5))
 plot_grid(MCC+guides(color="none", size="none"), nonMCC+guides(color="none", size="none"),
           get_legend(MCC), 
           rel_widths = c(1,1,0.5),
           ncol=3, align="hv", axis="t", labels=c("A", "B"))
-ggsave("github/figures/SuppFigure2.pdf", height=2.75, width=6.5)
+ggsave("github/figures/SuppFigure2.jpeg", height=2.75, width=6.5)
 
 # Supplemental Figure 3
 # See ddPCRcounts script 
@@ -250,3 +254,236 @@ pmcrA=ggplot(DF, aes(x=chl.a, y=copies_mcrA_per_g))+geom_point()+
   theme(panel.grid=element_blank())
 plot_grid(p16S,pmcrA, ncol=2, labels=c("A", "B"))
 ggsave("github/figures/SuppFigure4.pdf", height=3.25, width=6.5)
+
+
+# Get community composition barchart 
+library(tidyverse)
+library(reshape2)
+tax=tax %>% separate(col = "Taxonomy",sep=";", 
+                     into=c("Kingdom", "Phylum", "Class", "Order", "Genus", "Species"))
+motu.ra2=as.data.frame(t(motu.ra))
+motu.ra2$OTU=rownames(motu.ra2)
+motu.ra2=merge(motu.ra2, tax, by="OTU")
+motu.ra2$Species=gsub("\\([0-9]{2,3}\\)", "", motu.ra2$Species)
+motu.ra2$Species[motu.ra2$Species=="Methanomicrobiales_unclassified"]="Unclassified Methanomicrobia"
+motu.ra2$Species[motu.ra2$Species=="Methanomicrobia_unclassified"]="Unclassified Methanomicrobia"
+motu.ra2$Species[motu.ra2$Species=="Methanomicrobiales_incertae_sedis_unclassified"]="Unclassified Methanomicrobia"
+
+motu.ra2=motu.ra2[,c(2:23,30)]
+motu.ra2=aggregate(.~Species, data=motu.ra2, FUN=sum)
+rownames(motu.ra2)=motu.ra2$Species
+motu.ra2$Species=NULL
+motu.ra2=as.data.frame(t(motu.ra2))
+motu.ra2$LakeID=rownames(motu.ra2)
+motu.ra2$LakeID=gsub("3M_", "", motu.ra2$LakeID)
+motu.ra2=merge(motu.ra2, meta, by="LakeID")
+motu.ra2=motu.ra2[,c(1:10,13)]
+motu.ra2=melt(motu.ra2, id.vars = c("LakeID", "sediment_pH"), variable.name = "Genus", value.name = "RelAbund")
+
+ggplot(motu.ra2, aes(x=reorder(LakeID, sediment_pH), y=RelAbund))+
+  geom_col(aes(fill=Genus))+
+  scale_fill_brewer(palette = "Set3")+
+  xlab("Lake ID")+
+  ylab("Relative Abundance")+
+  theme(legend.position = "top", 
+        legend.key.width = unit(0.5, "cm"),
+        legend.key.height = unit(0.1, "cm"))+  
+  guides(fill=guide_legend(nrow=3,byrow=TRUE))
+ggsave("CH4response2OM/figures/methanogenComposition.pdf", height=3.5, width=6.5)
+
+# Phylum level analysis of non-methanogen community
+tax=tax %>% separate(col = "Taxonomy",sep=";", 
+                     into=c("Kingdom", "Phylum", "Class", "Order", "Genus", "Species"))
+botu.ra2=as.data.frame(t(botu.ra))
+botu.ra2$OTU=rownames(botu.ra2)
+botu.ra2=merge(botu.ra2, tax, by="OTU")
+botu.ra2$Phylum=gsub("\\([0-9]{2,3}\\)", "", botu.ra2$Phylum)
+
+botu.ra2=botu.ra2[,c(2:23,26)]
+botu.ra2=aggregate(.~Phylum, data=botu.ra2, FUN=sum)
+botu.ra2$Phylum[botu.ra2$Phylum=="Cyanobacteria_Chloroplast"]="Cyanobacteria"
+botu.ra2$Phylum[botu.ra2$Phylum=="Archaea_unclassified"]="Unclassified Archaea"
+botu.ra2$Phylum[botu.ra2$Phylum=="Bacteria_unclassified"]="Unclassified Bacteria"
+
+
+
+rownames(botu.ra2)=botu.ra2$Phylum
+botu.ra2$Phylum=NULL
+botu.ra2=as.data.frame(t(botu.ra2))
+botu.ra2$LakeID=rownames(botu.ra2)
+botu.ra2$LakeID=gsub("3M_", "", botu.ra2$LakeID)
+botu.ra2=merge(botu.ra2, meta, by="LakeID")
+botu.ra2=botu.ra2[,c(1:30,33)]
+botu.ra2=melt(botu.ra2, id.vars = c("LakeID", "sediment_pH"), variable.name = "Phylum", value.name = "RelAbund")
+
+ggplot(botu.ra2, aes(x=reorder(LakeID, sediment_pH), y=RelAbund))+
+  geom_col(aes(fill=Phylum))+
+  #scale_fill_brewer(palette = "Set3")+
+  xlab("Lake ID")+
+  ylab("Relative Abundance")+
+  theme(legend.position = "top", 
+        legend.key.width = unit(0.5, "cm"),
+        legend.key.height = unit(0.1, "cm"))+
+  guides(fill=guide_legend(nrow=10,byrow=TRUE))
+ggsave("CH4response2OM/figures/non-methanogenComposition.pdf", height=5, width=6.5)
+
+
+# Diversity analyses
+divM=diversity(motu.ra)
+divB=diversity(botu.ra)
+div=data.frame(LakeID=names(divM), divM=divM, divB=divB)
+div$LakeID=gsub("3M_", "", div$LakeID)
+DF=merge(DF, div, by="LakeID")
+
+
+# Richness
+DF$richM=NA
+DF$richB=NA
+
+for(i in 1:nrow(DF)){
+  # MCC
+  temp=motus[rownames(motus)==paste0("3M_", DF$LakeID[i]),]
+  DF$richM[i]=length(temp[temp>0])
+  # non-MCC
+  temp=botus[rownames(botus)==paste0("3M_", DF$LakeID[i]),]
+  DF$richB[i]=length(temp[temp>0])
+}
+range(divM)
+range(divB)
+
+range(DF$richM)
+range(DF$richB)
+
+summary(lm(log(CH4diff)~divM, data=DF))
+summary(lm(log(CH4diff)~divB, data=DF))
+summary(lm(log(CH4diff)~M.axis1, data=DF))
+summary(lm(log(CH4diff)~B.axis1, data=DF))
+summary(lm(log(CH4diff)~richB, data=DF))
+summary(lm(log(CH4diff)~richM, data=DF))
+summary(lm(log(CH4diff)~sediment_pH, data=DF))
+
+summary(lm(richM~sediment_pH, data=DF)) #no
+summary(lm(richM~percOM, data=DF)) #no
+summary(lm(richM~chl.a, data=DF)) #no
+summary(lm(richM~year, data=DF)) #no
+
+summary(lm(divM~sediment_pH, data=DF)) #no
+summary(lm(divM~percOM, data=DF)) #no
+summary(lm(divM~chl.a, data=DF)) #no
+summary(lm(divM~year, data=DF)) #no
+
+summary(lm(richB~sediment_pH, data=DF)) #yes
+summary(lm(richB~percOM, data=DF)) #yes
+summary(lm(richB~chl.a, data=DF)) #no
+summary(lm(richB~year, data=DF)) #no
+
+summary(lm(divB~sediment_pH, data=DF)) #yes
+summary(lm(divB~percOM, data=DF)) #yes
+summary(lm(divB~chl.a, data=DF)) #no
+summary(lm(divB~year, data=DF)) #no
+
+p2c=ggplot(DF, aes(x=divB, y=log(CH4diff)))+
+  stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  ylab(expression(paste("log(",Delta*CH[4]," production)")))+
+  xlab("non-MCC Shannon Index")+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = c(0.15,0.78),
+        legend.key.size = unit(0.3, "cm"),
+        legend.background = element_blank())+
+  scale_color_continuous(name="pH")
+p2c
+p2d=ggplot(DF, aes(x=divM, y=log(CH4diff)))+
+  stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  ylab(expression(paste("log(",Delta*CH[4]," production)")))+
+  xlab("MCC Shannon Index")+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  scale_color_continuous(name="pH")
+
+p2e=ggplot(DF, aes(x=richB, y=log(CH4diff)))+
+  stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  ylab(expression(paste("log(",Delta*CH[4]," production)")))+
+  xlab("non-MCC Richness")+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  scale_color_continuous(name="pH")
+
+p2f=ggplot(DF, aes(x=richM, y=log(CH4diff)))+
+  stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  ylab(expression(paste("log(",Delta*CH[4]," production)")))+
+  xlab("MCC Richness")+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  scale_color_continuous(name="pH")
+
+p2=plot_grid(p2c, 
+          p2d+guides(color="none"), 
+          p2e+guides(color="none"), 
+          p2f+guides(color="none"),
+          p2a+guides(color="none"), 
+          p2b+guides(color="none"),
+          nrow=2, 
+          align="hv", axis="r", labels = "AUTO",
+          byrow = F)
+ggsave("github/figures/Figure2.2.pdf", p2, height=5, width=6.5)
+
+
+DFfig=DF[,c(1,17,2,11,13,21:24)]
+DFfig=melt(DFfig, id.vars = c("LakeID", "sediment_pH", "CH4diff"), variable.name = "Metric")
+DFfig$Community=NA
+DFfig$Community[DFfig$Metric%in%c("M.axis1", "richM", "divM")]="Methanogen"
+DFfig$Community[!(DFfig$Metric%in%c("M.axis1", "richM", "divM"))]="Non-methanogen"
+DFfig$Metric=gsub("[BM][.]{0,1}", "", DFfig$Metric)
+
+DFfig$Metric=factor(DFfig$Metric,
+                    levels=c("div", "rich", "axis1"),
+                    labels=c("Shannon Index", "Richness", "PCoA Axis 1"))
+DFfig$Community=factor(DFfig$Community, 
+                       levels=c("Non-methanogen", "Methanogen"))
+p2=ggplot(DFfig, aes(x=value, y=log(CH4diff)))+
+  #stat_smooth(data=DFfig[!(DFfig$Community=="Methanogen"&DFfig$Metric=="Richness"),], method="lm", formula=y~x, color="black")+
+  stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  ylab(expression(paste("log(",Delta*CH[4]," production)")))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        strip.placement = "outside",
+        axis.title.x = element_blank(),
+        strip.background =element_blank(),
+        strip.text = element_text(size=10))+
+  scale_color_continuous(name="pH")+
+  facet_wrap(Community~Metric, scales = "free_x", strip.position = "bottom")
+p2
+ggsave("github/figures/Figure2.2.jpeg", p2, height=5, width=6.5)
+
+ggplot(DF, aes(x=sediment_pH, y=meth_relAbund*100))+
+  stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  ylab("Methanogen relative abundance (%)")+
+  xlab("Sediment pH")+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  scale_color_continuous(name="pH")
+
+summary(lm(log(CH4diff)~meth_relAbund, data=DF))
+summary(lm(meth_relAbund~sediment_pH, data=DF))
+
+ggplot(DF, aes(y=log(CH4diff), x=meth_relAbund*100))+
+  #stat_smooth(method="lm", formula=y~x, color="black")+
+  geom_point(aes(color=sediment_pH))+
+  xlab("Methanogen relative abundance (%)")+
+  ylab(expression(paste("log(",Delta*CH[4]," production)")))+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  scale_color_continuous(name="pH")
+
+summary(lm(sediment_pH~divM, data=DF))
+summary(lm(sediment_pH~divB, data=DF))
+summary(lm(sediment_pH~richM, data=DF))
+summary(lm(sediment_pH~richB, data=DF))
+
